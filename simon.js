e99a0ff43aaa15ff66c50bcd40703c145ee216f9
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-05-13 22:52:45 +1200

    color change timer

--- simon.js
+++ simon.js
@@ -7,7 +7,27 @@ var red = document.getElementsByClassName("red")[0];
 var yellow = document.getElementsByClassName("yellow")[0];
 
 yellow.addEventListener("click", function(){
-  yellow.style.backgroundColor = "#E2BA00";
+  yellow.style.backgroundColor = "#E2BA00"; //change bg color on click
+  setTimeout(function() {
+  yellow.style.backgroundColor = "yellow"; //change bg color back after
+}, 500);                                   //half a second
 });
-
+green.addEventListener("click", function(){
+  green.style.backgroundColor = "#004400";
+  setTimeout(function() {
+  green.style.backgroundColor = "green";
+}, 500);
+});
+red.addEventListener("click", function(){
+  red.style.backgroundColor = "#76322B";
+  setTimeout(function() {
+  red.style.backgroundColor = "red";
+}, 500);
+});
+blue.addEventListener("click", function(){
+  blue.style.backgroundColor = "#5054B4";
+  setTimeout(function() {
+  blue.style.backgroundColor = "blue";
+}, 500);
+});
 }
