470e82a083fc8fd081c39dd015ac7c942fbd1095
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-05-13 23:52:08 +1200

    fix random no mistake

--- simon.js
+++ simon.js
@@ -46,7 +46,7 @@ blue.addEventListener("click", function(){
 });
 
 function startSequence(){
-var randomNo = numArr[Math.floor(Math.random() * 3)];
+var randomNo = Math.floor(Math.random() * 4);
 sequence.push(numArr[randomNo]);
 messageBoard.innerHTML = sequence;
 playColors();
@@ -58,22 +58,22 @@ function playColors(){
       yellow.style.backgroundColor = "#E2BA00";
       setTimeout(function() {
       yellow.style.backgroundColor = "yellow";
-      }, 500);
-    } if (sequence[i] === 2){
+    }, 1000);
+  } if (sequence[i] === 2){
       green.style.backgroundColor = "#004400";
       setTimeout(function() {
       green.style.backgroundColor = "green";
-      }, 500);
-    } if (sequence === 3){
+    }, 1000);
+  } if (sequence === 3){
       red.style.backgroundColor = "#76322B";
       setTimeout(function() {
       red.style.backgroundColor = "red";
-      }, 500);
-    } if (sequence[i] === 4){
+    }, 1000);
+  } if (sequence[i] === 4){
       blue.style.backgroundColor = "#5054B4";
       setTimeout(function() {
       blue.style.backgroundColor = "blue";
-      }, 500);
+    }, 1000);
     }
   }
 }
